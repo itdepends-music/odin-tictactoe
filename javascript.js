b71f46f8b0b5a61fd25b908d2674a248fdf1f5b2
Oscar Robertson <86749444+orthesavage@users.noreply.github.com>
2023-04-06 21:58:55 +1200

    Make getBoard public scoped

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-const gameBoardController = (() => {
+const gameBoard = (() => {
   const board = Array(3)
   for (let i = 0; i < board.length; i++) {
     board[i] = [0, 0, 0]
@@ -7,4 +7,6 @@ const gameBoardController = (() => {
   const getBoard = () => {
     return board.map(elem => [...elem]) // return deep copy of board
   }
+
+  return { getBoard }
 })()
