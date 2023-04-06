fd377a0d12f16249d8d9e7aa0a64372e110fc941
Oscar Robertson <86749444+orthesavage@users.noreply.github.com>
2023-04-06 21:58:27 +1200

    Add module gameBoard to manage the game board

---
+++ javascript.js
@@ -0,0 +1,10 @@
+const gameBoardController = (() => {
+  const board = Array(3)
+  for (let i = 0; i < board.length; i++) {
+    board[i] = [0, 0, 0]
+  }
+
+  const getBoard = () => {
+    return board.map(elem => [...elem]) // return deep copy of board
+  }
+})()
