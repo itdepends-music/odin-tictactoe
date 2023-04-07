33f96f3b9a93f72427ca24da3de2ffe2ef5a08da
Oscar Robertson <86749444+orthesavage@users.noreply.github.com>
2023-04-07 21:26:15 +1200

    Add display module to update screen

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 const board = (() => {
   const board = Array(3)
   for (let i = 0; i < board.length; i++) {
-    board[i] = [' ', ' ', ' ']
+    board[i] = ['X', 'O', ' ']
   }
 
   const getBoard = () => {
@@ -9,4 +9,33 @@ const board = (() => {
   }
 
   return { getBoard }
+})()
+
+const display = (() => {
+  const boardDiv = document.querySelector('.board')
+
+  const update = () => {
+    // clear previous content from boardDiv
+    while (boardDiv.firstChild) {
+      boardDiv.removeChild(boardDiv.firstChild)
+    }
+
+    for (const row of board.getBoard()) {
+      const rowDiv = document.createElement('div')
+      rowDiv.classList.add('row')
+      for (const square of row) {
+        const squareDiv = document.createElement('button')
+        squareDiv.classList.add('square')
+        squareDiv.textContent = square
+        if (squareDiv.textContent !== ' ') {
+          squareDiv.disabled = true
+        }
+        rowDiv.appendChild(squareDiv)
+      }
+      boardDiv.appendChild(rowDiv)
+    }
+  }
+
+  update()
+  update()
 })()
